8256ee32b6caa0200197efd750ad1b521344caa6
sivefunc <sivefunc@tuta.io>
2025-06-03 16:29:57 -0400

    feat: letter adding

--- src/ambigram.py
+++ src/ambigram.py
@@ -1,15 +1,50 @@
 import cadquery as cq
 from cadquery.vis import show
 
+from utils import merge_strings
 from _version import __version__
 
 class Ambigram(object):
-    workplane = None
+    assembly = None
+    merged_string = None
+
     def __init__(self, first_text: str, second_text: str):
-        pass
+        self.assembly = cq.Assembly()
+        self.merged_string = merge_strings(first_text, second_text)
+
+        location = (0, 0, 0)
+        for (short_char, *long_chars) in self.merged_string:
+            bbox = None
+            for char in long_chars + [short_char]:
+                x, y, z = location
+
+                letter = cq.Workplane("XZ").text(
+                    char,
+                    1,
+                    0.5,
+                    halign="left",
+                    valign="bottom"
+                )
+
+                bbox = letter.val().BoundingBox()
+
+                self.assembly.add(
+                    letter,
+                    loc=cq.Location(*[
+                        x - bbox.xmin,
+                        y - bbox.ymin,
+                        z - bbox.zmin
+                    ])
+                )
+
+                location = x, y - bbox.ylen, z
+
+            x, y, z = location
+            location = x + bbox.xlen, y, z
 
 def main():
-    show(cq.Workplane("XY").text(f"{__version__}", 10, 2))
+    ambigram = Ambigram("recursive", "function")
+    show(ambigram.assembly)
 
 if __name__ == "__main__":
     main()
