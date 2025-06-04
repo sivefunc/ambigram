82171f3e42a743eb31545717ce185e5887b20143
sivefunc <sivefunc@tuta.io>
2025-06-04 18:32:32 -0400

    docs: documenting code

--- src/ambigram.py
+++ src/ambigram.py
@@ -5,14 +5,84 @@ from utils import merge_strings
 from _version import __version__
 
 class Ambigram(object):
+    """3D Ambigram Generation using CadQuery
+
+    A 3D Ambigram is a optical illusion where two texts gets merged
+    and depending of the perspective one text appears and the other
+    don't.
+
+    Attributes
+    ----------
+    assembly : None | cadquery.Assembly
+               3D Ambigram of Two Strings
+
+    merged_string : None | list[list[str]]
+                    The merged string that was used to create the ambigram
+
+    See Also
+    --------
+    utils.merge_strings : Function that merges two strings
+    """
+
     assembly = None
     merged_string = None
-
+    
     def __init__(self,
                  first_text: str,
                  second_text: str,
                  font_path: str = None,
                  ):
+        """Creates the 3D Ambigram in a Cadquery Assembly Object
+
+        2D View of an Ambigram:
+        first_text: VERYLONG
+        second_text: SHORT
+
+        y(+)
+        ._______x(+)
+        | V
+        | E
+        | S
+        |  R
+        |  Y
+        |  H
+        |   L
+        |   O
+        |   O
+        |    N
+        |    R
+        |     G
+        |     T
+        y(-)
+
+        Parameters
+        ----------
+        first_text : str
+                     Non-empty unsorted string.
+
+        second_text : str
+                      Non-empty unsorted string.
+
+        font_path : str, optional
+                    Path to the font that the text is going to use
+
+        Notes
+        -----
+        The Ambigram has two perspectives the Positive X Line and the
+        Negative Y Line, each one displays a different string.
+
+        The Algorithms works by intersecting a pair of letters at a time
+        and placing them at the assembly instead of placing all letters and
+        then intersecting at once.
+
+        The Insersectiong of a Pair of Letters is by rotating 90 deg a
+        letter in the Z direction and then extruding both letters.
+
+        See Also
+        --------
+        utils.merge_strings : Function that merges two strings
+
+        """
 
         self.first_text = first_text
         self.second_text = second_text
@@ -24,50 +94,66 @@ class Ambigram(object):
         location = (0, 0, 0)
         for (short_char, *long_chars) in self.merged_string:
             bb_int = None
+
+            # Intersect each pair of letters
             for long_char in long_chars:
                 x, y, z = location
 
+                # Letter that will be visible in the Positive X Line
                 xline = cq.Workplane("XZ").text(
-                    short_char,
-                    1,
-                    1,
+                    txt=short_char,
+                    fontsize=1,
+                    distance=1,
                     halign="left",
                     valign="bottom",
                     fontPath=self.font_path
                 )
 
+                # Letter that will be visible in the Negative Y Line
                 yline = cq.Workplane("XZ").text(
-                    long_char,
-                    1,
-                    1,
+                    txt=long_char,
+                    fontsize=1,
+                    distance=1,
                     halign="left",
                     valign="bottom",
                     fontPath=self.font_path,
                 ).rotate([0,0,0],[0,0,1], -90)
 
+                # All Intersections are done at [0, 0, 0] then are translated
+                # to their corresponding position, forming a diagonal.
+                # y(+)
+                # |
+                # |
+                # |
+                # |_______ x(+)
+                # 0
+
+                # Bounding Box
                 bbx = xline.val().BoundingBox()
                 bby = yline.val().BoundingBox()
-                
+
                 # Text() objects are away from origin (0, 0, 0) so we have to
                 # do this weird trick of reversing their displacement.
                 # translate(0,0,0) won't work (amazing I know)
                 xline = xline.translate([-bbx.xmin,-bbx.ymin,-bbx.zmin])
                 yline = yline.translate([-bby.xmin,-bby.ymin,-bby.zmin])
-
+                
                 intersection = xline.intersect(yline)
+
                 bb_int = intersection.val().BoundingBox()
 
                 location = x, y - bb_int.ylen, z
                 intersection = intersection.translate(location)
-                self.assembly.add(intersection)
+
+                self.assembly = self.assembly.add(intersection)
 
             x, y, z = location
             location = x + bb_int.xlen, y, z
 
 def main():
     ambigram = Ambigram(
-        "HOLIWIS",
-        "CAMIONX",
+        "AVERYLONGWORD",
+        "SHORT",
         font_path="/usr/share/fonts/truetype/ibm-plex/IBMPlexSans-Bold.ttf"
     )
     show(ambigram.assembly)
